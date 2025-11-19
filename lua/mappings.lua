736cc1c9eb4ff93e00d0cf901cdb2bf82f14e3fe
MrPaolinsky <paolinskypdtn@gmail.com>
2025-11-19 00:21:35 -0500

    my own base configs for C

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -8,3 +8,9 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+-- nvimtree (Remove default toggle)
+vim.keymap.del("n", "<C-n>")
+map("n", "<C-b>", "<cmd>NvimTreeToggle<CR>", { desc = "nvimtree toggle window" })
+map("n", "<leader>e", "<cmd>NvimTreeFocus<CR>", { desc = "nvimtree focus window" })
+
+
--- lua/options.lua
+++ lua/options.lua
@@ -4,3 +4,5 @@ require "nvchad.options"
 
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
+vim.opt.shiftwidth=4
+vim.opt.tabstop=4
