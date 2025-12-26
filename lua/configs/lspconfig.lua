79ebaf029872f0da2f11922c57f708e4d903916c
MrPaolinsky <paolinskypdtn@proton.me>
2025-12-25 20:10:06 -0500

    fix broken lsp config

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,6 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls", "clangd", "lua-language-server", "gopls", "rust-analyzer", "pyright", "deno" }
+local servers = { "html", "cssls", "clangd", "lua-language-server", "gopls", "rust_analyzer", "pyright", "denols" }
 vim.lsp.enable(servers)
 
 -- read :h vim.lsp.config for changing options of lsp servers
